5b64b7765509f93b7e2a13a101135f9040db724c
rasta-masta <143915828+rasta-masta@users.noreply.github.com>
2023-09-24 23:13:03 +0700

    editing todo_list

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Flex, Text, Button, Input } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Flex w="100%" h="100vh">
+        <Flex 
+        w="100%" 
+        flexDir="column" 
+        ml="20%" 
+        mt="5%" 
+        mr="20%"
+        color="white">
+          <Text fontWeight="700" fontSize="30">Tasks</Text>
+          <Flex mt="10%">
+            <Input variant="flushed" placeholder='Add Task' width="60%"/>
+            <Button ml="5" bg="cyan.800">Add Task</Button>
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   )
 }
--- todo_list/src/main.jsx
+++ todo_list/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react'
+
+// Define the theme with initialColorMode set to 'dark'
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
